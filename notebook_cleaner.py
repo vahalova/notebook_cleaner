541c449ddc21e08c9453172e6236c2234159362c
Kotatko <vahalova@gmail.com>
2018-02-11 17:25:14 +0100

    kontrola verze

--- notebook_cleaner.py
+++ notebook_cleaner.py
@@ -1,8 +1,14 @@
 import json
+
+def clean_notebook(data):
+    if data["nbformat"] != 4 or data["nbformat_minor"] <2:
+        raise ValueError("bad version of notebook")
+
+
 with open("test_data.ipynb", encoding="utf-8") as soubor:
     obsah = soubor.read()
 data = json.loads(obsah)
-
+clean_notebook(data)
 
 vysledek = json.dumps(data, ensure_ascii=False, indent=2 )
 print(vysledek)
