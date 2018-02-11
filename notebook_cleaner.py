c64322930b83e2050bb0af0499df8672fe0aefbc
Kotatko <vahalova@gmail.com>
2018-02-11 18:11:44 +0100

    change execution count

--- notebook_cleaner.py
+++ notebook_cleaner.py
@@ -3,12 +3,13 @@ import json
 def clean_notebook(data):
     if data["nbformat"] != 4 or data["nbformat_minor"] <2:
         raise ValueError("bad version of notebook")
-
+    for cell in data["cells"]:
+        cell["execution_count"] = None
 
 with open("test_data.ipynb", encoding="utf-8") as input_file:
     content = input_file.read()
 data = json.loads(content)
 clean_notebook(data)
 
-result = json.dumps(data, ensure_ascii=False, indent=2 )
+result = json.dumps(data, ensure_ascii=False, indent=4 )
 print(result)
