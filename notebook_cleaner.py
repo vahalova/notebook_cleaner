e8badd9465020189d8f95285741081b039ab1b50
Kotatko <vahalova@gmail.com>
2018-02-11 16:56:14 +0100

    prevedeni zpet na retezec

--- notebook_cleaner.py
+++ notebook_cleaner.py
@@ -2,4 +2,7 @@ import json
 with open("test_data.ipynb", encoding="utf-8") as soubor:
     obsah = soubor.read()
 data = json.loads(obsah)
-print(data)
+
+
+vysledek = json.dumps(data, ensure_ascii=False, indent=2 )
+print(vysledek)
