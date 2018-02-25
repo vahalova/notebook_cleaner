67f57e8108d615b4ae2dbfbb34d6b9cb0407d0e3
Kotatko <vahalova@gmail.com>
2018-02-25 21:58:54 +0100

    add click command line interface

--- notebook_cleaner.py
+++ notebook_cleaner.py
@@ -1,9 +1,19 @@
 import json
+import click
+
+@click.command()
+@click.argument("file", nargs=1)
+def main(file):
+    with open(file, encoding="utf-8") as input_file:
+        content = input_file.read()
+    data = json.loads(content)
+    clean_notebook(data)
+    result = json.dumps(data, ensure_ascii=False, indent=4)
+    print(result)
 
 def clean_notebook(data):
-    if data["nbformat"] != 4 or data["nbformat_minor"] <2:
+    if data["nbformat"] != 4 or data["nbformat_minor"] < 2:
         raise ValueError("bad version of notebook")
-
     for cell in data["cells"]:
         if "execution_count" in cell:
             cell["execution_count"] = None
@@ -11,10 +21,4 @@ def clean_notebook(data):
             if "execution_count" in output:
                 output["execution_count"] = None
 
-with open("test_data.ipynb", encoding="utf-8") as input_file:
-    content = input_file.read()
-data = json.loads(content)
-clean_notebook(data)
-
-result = json.dumps(data, ensure_ascii=False, indent=4 )
-print(result)
+main()
