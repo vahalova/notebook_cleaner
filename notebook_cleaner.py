c1a1af5eeff23f0863ad3fd87877aaff8e427610
Kotatko <vahalova@gmail.com>
2018-03-15 15:52:36 +0100

    add output

--- notebook_cleaner.py
+++ notebook_cleaner.py
@@ -3,14 +3,15 @@ import click
 
 @click.command()
 @click.argument("files", nargs=-1)
-@click.option('-i/','--inplace/--no-inplace', help="overwrite the file with its cleaned-up version")
-def main(files, inplace):
+@click.option('-i/','--inplace/--no-inplace', help="overwrite the file/s with its cleaned-up version")
+@click.option('--output', type = click.File(file_okay=False, dir_okay=True, writable=True), help="save the cleaned-up file/s in a directory")
+def main(files, inplace, output):
     for file in files:
         with open(file, encoding="utf-8") as input_file:
             content = input_file.read()
         data = json.loads(content)
         clean_notebook(data)
-        result = json.dumps(data, ensure_ascii=False, indent=1)   
+        result = json.dumps(data, ensure_ascii=False, indent=1)
         if inplace:
             with open(file, "w", encoding="utf-8") as output_file:
                 output_file.write(result)
